9ff5bcd910e797433dda7edf56dda9d0bf7e8d3e
youngunyou <ygyou@me.com>
2019-10-15 19:51:35 +0800

    first commt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navi from './components/Navi'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
-    </div>
-  );
+    );
+  }
 }
 
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title="WEB2" sub="www!"></Subject>
+        <Navi></Navi>
+        <Content title="html" desc="html is hypertext markup language."></Content>
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/components/Navi.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Navi extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+          <li><a href="1.html">html</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">java script</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+export default Navi;
